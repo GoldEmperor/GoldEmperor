dbe3a12565e1d392bac648571b716aa1e43fc13c
CYZ <249876616@>
2017-08-12 17:04:34 +0800

    测试

---
+++ app/src/main/java/com/goldemperor/tet/test.java
@@ -0,0 +1,8 @@
+package com.goldemperor.tet;
+
+/**
+ * Created by Nova on 2017/8/12.
+ */
+
+public class test {
+}
--- app/src/main/java/com/goldemperor/text/text.java
+++
@@ -1,8 +0,0 @@
-package com.goldemperor.text;
-
-/**
- * Created by Nova on 2017/8/12.
- */
-
-public class text {
-}
